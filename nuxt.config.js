3fcca35bed9409a2c5d7821f8e05305e7310c199
sean <seanmilburn1990@gmail.com>
2023-05-12 16:25:32 +0200

    Rebuild

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'sam-and-ll',
+    title: 'Sam & LL',
     htmlAttrs: {
       lang: 'en',
     },
